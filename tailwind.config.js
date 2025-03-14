62cfacd33140b2ad3d006a6887c641711481f976
Lika <lika.likuna2008@gmail.com>
2025-03-14 18:29:27 +0400

    feat: added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        fredokaOne: ["Fredoka One", "sans-serif"],
+        firaGo: ["FiraGO", "sans-serif"],
+      },
+      colors: {},
+    },
   },
   plugins: [],
 };

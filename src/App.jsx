93a045c10a73d14a4abe2693c35d065d8c920eae
Lika <lika.likuna2008@gmail.com>
2025-03-15 01:06:42 +0400

    Installed react-router-dom, configured routing, and created task pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from "./pages/Home";
 import Header from "./components/layout/Header";
+import Layout from "./components/layout/Layout";
+
+import TasksList from "./pages/tasks/TasksList";
+import TaskDetails from "./pages/tasks/TaskDetails";
+import CreateTask from "./pages/tasks/CreateTask";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<Home />} />
+      <Route path="tasks">
+        <Route index element={<TasksList />} />
+        <Route path=":id" element={<TaskDetails />} />
+        <Route path="create" element={<CreateTask />} />
+      </Route>
+    </Route>
+  )
+);
 
 function App() {
   return (
     <>
       <Header />
+      <Layout>
+        <RouterProvider router={router} />
+      </Layout>
     </>
   );
 }
---
+++ src/pages/tasks/TasksList.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TasksList() {
+  return <div>დამატებული დავალებების გვერდი</div>;
+}

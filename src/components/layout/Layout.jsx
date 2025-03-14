96c235f9b0b4baa5c1c71337aa5b143d34cad0a3
Lika <lika.likuna2008@gmail.com>
2025-03-15 00:38:03 +0400

    feat: created layout component

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,9 @@
+const Layout = ({ children }) => {
+  return (
+    <div className="w-full max-w-[87.5%] m-auto mt-[40px] pb-[152px]">
+      {children}
+    </div>
+  );
+};
+
+export default Layout;

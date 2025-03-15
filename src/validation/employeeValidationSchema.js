313f0fe23075884a65d98cf9d851f7afe1cf940f
Lika <lika.likuna2008@gmail.com>
2025-03-16 01:51:04 +0400

    feat: added more validations for employee

--- src/validation/employeeValidationSchema.js
+++ src/validation/employeeValidationSchema.js
@@ -4,16 +4,23 @@ export const employeeValidationSchema = Yup.object().shape({
   name: Yup.string()
     .required("სახელი სავალდებულოა")
     .min(2, "მინიმუმ 2 სიმბოლო")
-    .max(255, "მაქსიმუმ 255 სიმბოლო"),
+    .max(255, "მაქსიმუმ 255 სიმბოლო")
+    .matches(/^[a-zA-Zა-ჰ]+$/, " მხოლოდ ლათინური და ქართული სიმბოლოები "),
   surname: Yup.string()
     .required("გვარი სავალდებულოა")
     .min(2, "მინიმუმ 2 სიმბოლო")
-    .max(255, "მაქსიმუმ 255 სიმბოლო"),
+    .max(255, "მაქსიმუმ 255 სიმბოლო")
+    .matches(/^[a-zA-Zა-ჰ]+$/, " მხოლოდ ლათინური და ქართული სიმბოლოები "),
   avatar: Yup.mixed()
     .required("ავატარი სავალდებულოა")
     .test(
       "fileType",
       "ავატარი უნდა იყოს სურათის ტიპის",
       (value) => value && value.type.startsWith("image/")
+    )
+    .test(
+      "fileSize",
+      "მაქსიმუმ 600KB",
+      (value) => value && value.size <= 600000
     ),
 });

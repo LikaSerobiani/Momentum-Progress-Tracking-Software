46327a48ae3c0df6f6ef0dbfa6d2726c5d54102f
Lika <lika.likuna2008@gmail.com>
2025-03-16 18:02:09 +0400

    feat: added new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,10 +17,10 @@ export default {
         red: "#FA4D4D",
         borderGray: "#CED4DA",
         gray: {
-          10: "", // 1%
-          100: "#DEE2E6", // 10% +
+          10: "#F8F9FA", // 1%
+          100: "#DEE2E6", // 10%
           200: "", // 15%
-          300: "", // 20%
+          300: "#ADB5BD", // 20%
           greyish: "",
           headline: "#212529",
           validation: "#6C757D",

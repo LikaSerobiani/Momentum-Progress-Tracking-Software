5625eb88f9bc5c34eccced0df5f0b8aa5f65c131
Lika <lika.likuna2008@gmail.com>
2025-03-16 04:15:51 +0400

    feat: added icons

---
+++ src/components/common/icons/ArrowDown.jsx
@@ -0,0 +1,22 @@
+export default function ArrowDown() {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="12"
+        height="7"
+        viewBox="0 0 12 7"
+        fill="none"
+      >
+        <path
+          d="M10.6199 1.72083L6.81655 5.52416C6.36738 5.97333 5.63238 5.97333 5.18322 5.52416L1.37988 1.72083"
+          stroke="#343A40"
+          strokeWidth="1.5"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </div>
+  );
+}

0031d211ea50c33ec85ed89249c81b1e13e39926
Lika <lika.likuna2008@gmail.com>
2025-03-16 04:05:20 +0400

    task: changed icon

--- src/components/common/icons/Check.jsx
+++ src/components/common/icons/Check.jsx
@@ -3,17 +3,17 @@ export default function Check({ color }) {
     <div>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="12"
-        height="11"
-        viewBox="0 0 12 11"
+        width="14"
+        height="10"
+        viewBox="0 0 14 10"
         fill="none"
       >
         <path
-          d="M11 1.40918L4.125 9.591L1 5.87199"
+          d="M12.3334 1.33325L5.00008 8.66659L1.66675 5.33325"
           stroke={color}
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         />
       </svg>
     </div>

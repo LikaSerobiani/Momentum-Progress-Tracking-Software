ef77f2eb5b464720dc3d7d6c56de957743d2fc00
Lika <lika.likuna2008@gmail.com>
2025-03-16 04:04:55 +0400

    feat: created checkBox component

---
+++ src/components/common/CheckBox/index.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import Check from "../Icons/Check";
+
+const Checkbox = ({ id, label, checked = false, onChange }) => {
+  const [isChecked, setIsChecked] = useState(checked);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+    onChange(!isChecked);
+  };
+
+  return (
+    <div className="flex items-center gap-[10px]">
+      <div
+        className="relative w-[22px] h-[22px] border-[1.5px] border-gray-headline rounded-md cursor-pointer"
+        onClick={handleCheckboxChange}
+      >
+        {isChecked && (
+          <div className="absolute inset-0 flex items-center justify-center">
+            <Check color="#212529" />
+          </div>
+        )}
+      </div>
+      {label && (
+        <label
+          htmlFor={id}
+          className="text-gray-headline text-[16px] font-normal font-firaGo"
+        >
+          {label}
+        </label>
+      )}
+    </div>
+  );
+};
+
+Checkbox.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  checked: PropTypes.bool,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Checkbox;

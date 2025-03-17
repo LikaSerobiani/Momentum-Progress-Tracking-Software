bce6b58700c84643e6b34eefc588e133d8b258a9
Lika <lika.likuna2008@gmail.com>
2025-03-17 16:58:33 +0400

    fix: removed title from task list

--- src/pages/tasks/TaskList.jsx
+++ src/pages/tasks/TaskList.jsx
@@ -16,24 +16,19 @@ export default function TaskList({ tasks }) {
   }
 
   return (
-    <div>
-      <h2 className="font-firaGo font-bold text-[34px] leading-[100%] text-gray-headline mb-[25px]">
-        დავალებების გვერდი
-      </h2>
-      <div className="flex flex-wrap gap-[52px]">
-        {tasks.map((task) => (
-          <Card
-            key={task.id}
-            id={task.id}
-            name={task.name}
-            description={task.description}
-            due_date={task.due_date}
-            department={task.department}
-            employee={task.employee}
-            priority={task.priority}
-          />
-        ))}
-      </div>
+    <div className="flex flex-wrap gap-[52px]">
+      {tasks.map((task) => (
+        <Card
+          key={task.id}
+          id={task.id}
+          name={task.name}
+          description={task.description}
+          due_date={task.due_date}
+          department={task.department}
+          employee={task.employee}
+          priority={task.priority}
+        />
+      ))}
     </div>
   );
 }

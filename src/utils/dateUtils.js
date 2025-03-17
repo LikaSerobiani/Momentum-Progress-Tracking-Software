1c5de22db8bb084284a16ad7827c5514bd0b438e
Lika <lika.likuna2008@gmail.com>
2025-03-18 00:27:21 +0400

    feat: formatted date with weekday

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -5,3 +5,8 @@ export const formatDueDate = (dateString) => {
   const date = new Date(dateString);
   return format(date, "dd MMM, yyyy", { locale: ka });
 };
+
+export const formatDueDateWithWeekday = (dateString) => {
+  const date = new Date(dateString);
+  return format(date, "EEE - dd/M/yyyy", { locale: ka });
+};

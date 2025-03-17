d323aa8f50f1027e8d7679690864f40c83a1af44
Lika <lika.likuna2008@gmail.com>
2025-03-17 04:29:00 +0400

    feat: displayed task list on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useTaskStore from "../stores/useTaskStore";
+import TaskList from "./tasks/TaskList";
 
 export default function Home() {
-  return <div>home</div>;
+  const { tasks, fetchTasks } = useTaskStore();
+
+  useEffect(() => {
+    fetchTasks();
+  }, [fetchTasks]);
+
+  return (
+    <div>
+      <TaskList tasks={tasks} />
+    </div>
+  );
 }

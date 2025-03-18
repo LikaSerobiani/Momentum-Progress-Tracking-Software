6d973068f26b1ef108267555a4ef2423e6d80f03
Lika <lika.likuna2008@gmail.com>
2025-03-18 04:18:44 +0400

    feat: updated task status from selector, cleared code and added comment section

--- src/pages/tasks/TaskDetails.jsx
+++ src/pages/tasks/TaskDetails.jsx
@@ -1,63 +1,81 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { formatDueDateWithWeekday } from "../../utils/dateUtils";
 
 import PriorityTag from "../../components/specific/task/PriorityTag";
 import DepartmentTag from "../../components/specific/task/DepartmentTag";
 import Loading from "../../components/common/Loading";
-
+import Selector from "../../components/common/Selector";
 import useTaskStore from "../../stores/UseTaskStore";
-
-import User from "../../components/common/Icons/User";
-import Calendar from "../../components/common/Icons/Calendar";
-import PieChart from "../../components/common/Icons/PieChart";
+import useStatusStore from "../../stores/useStatusStore";
+import UserIcon from "../../components/common/Icons/User";
+import CalendarIcon from "../../components/common/Icons/Calendar";
+import PieChartIcon from "../../components/common/Icons/PieChart";
+import CommentSection from "../../components/specific/task/CommentSection";
 
 export default function TaskDetails() {
   const { id } = useParams();
 
-  const { currentTask, fetchCurrentTask, fetchTasks } = useTaskStore();
+  const { currentTask, fetchCurrentTask, fetchTasks, updateTaskStatus } =
+    useTaskStore();
+  const { statuses, fetchStatuses } = useStatusStore();
+
+  const [selectedStatus, setSelectedStatus] = useState("");
 
   useEffect(() => {
     if (id) {
       fetchCurrentTask(id);
     }
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   }, [id, fetchCurrentTask]);
 
   useEffect(() => {
     fetchTasks();
-  }, [fetchTasks]);
+    fetchStatuses();
+  }, [fetchTasks, fetchStatuses]);
 
-  if (!currentTask) {
-    return <Loading />;
-  }
+  useEffect(() => {
+    if (currentTask && currentTask.status) {
+      setSelectedStatus(currentTask.status.name);
+    }
+  }, [currentTask]);
 
-  const {
-    name: taskName,
-    description,
-    due_date,
-    priority,
-    employee: { avatar, name: employeeName, surname, department } = {},
-  } = currentTask;
+  const handleStatusChange = async (newStatus) => {
+    const selectedStatusObj = statuses.find((s) => s.name === newStatus);
+    if (!selectedStatusObj) return;
 
-  console.log(currentTask);
+    try {
+      await updateTaskStatus(id, selectedStatusObj.id);
+      fetchCurrentTask(id);
+      setSelectedStatus(newStatus);
+    } catch (error) {
+      console.error("Failed to update task status:", error.message);
+    }
+  };
+
+  if (!currentTask) return <Loading />;
+
+  const { name, description, due_date, priority, employee } = currentTask;
+  const { avatar, name: employeeName, surname, department } = employee || {};
+
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-between">
       <div className="flex flex-col gap-[63px]">
         {/* description */}
         <div className="flex flex-col gap-3">
           <div className="flex items-center gap-[18px]">
-            <PriorityTag label={priority.name} icon={priority.icon} />
-            <DepartmentTag label={department.name} />
+            <PriorityTag label={priority?.name} icon={priority?.icon} />
+            {department && <DepartmentTag label={department.name} />}
           </div>
           <div className="flex flex-col gap-[26px]">
             <p className="text-[34px] font-semibold text-firaGo text-gray-headline">
-              {taskName}
+              {name}
             </p>
-            <p className="text-[18px] font-normal text-black text-firaGo">
+            <p className="text-[18px] font-normal text-black text-firaGo w-[715px]">
               {description}
             </p>
           </div>
         </div>
+
         {/* details */}
         <div className="flex flex-col gap-[18px]">
           <p className="text-[24px] text-firaGo font-semibold">
@@ -66,43 +84,67 @@ export default function TaskDetails() {
           <div className="flex flex-col gap-7">
             {/* status */}
             <div className="flex items-center gap-[70px]">
-              <div className="flex items-center gap-[6px]">
-                <PieChart />
-                <span className="text-[16px] text-firaGo ">სტატუსი</span>
+              <div className="flex items-center gap-[6px] w-[164px]">
+                <PieChartIcon />
+                <span className="text-[16px] text-firaGo leading-[150%]">
+                  სტატუსი
+                </span>
               </div>
+              <Selector
+                name="status_id"
+                id="status_id"
+                options={statuses.map((status) => status.name)}
+                selectedOption={selectedStatus}
+                onSelect={handleStatusChange}
+                width="w-[259px]"
+              />
             </div>
-            {/* user */}
-            <div className="flex items-center gap-[70px]">
-              <div className="flex items-center gap-[6px]">
-                <User />
-                <span className="text-[16px] text-firaGo">თანამშრომელი</span>
-              </div>
-              <div className="flex gap-3 items-center">
-                <img
-                  src={avatar}
-                  alt={`${employeeName} ${surname}`}
-                  className="w-8 h-8 rounded-full object-cover"
-                />
-                <div>
-                  <p className="text-gray-200 text-firaGo text-xs">
-                    {department.name}
-                  </p>
-                  <p className="text-gray-blackish text-[14px] text-firaGo font-normal">
-                    <span>{employeeName}</span> <span>{surname}</span>
-                  </p>
+
+            {/* assigned employee */}
+            {employee && (
+              <div className="flex items-center gap-[70px]">
+                <div className="flex items-center gap-[6px] w-[164px]">
+                  <UserIcon />
+                  <span className="text-[16px] text-firaGo leading-[150%] font-normal">
+                    თანამშრომელი
+                  </span>
+                </div>
+                <div className="flex gap-3 items-center">
+                  <img
+                    src={avatar}
+                    alt={`${employeeName} ${surname}`}
+                    className="w-8 h-8 rounded-full object-cover"
+                  />
+                  <div>
+                    <p className="text-gray-200 text-firaGo text-[11px] leading-[100%]">
+                      {department?.name}
+                    </p>
+                    <p className="text-gray-blackish text-[14px] text-firaGo font-normal leading-[150%]">
+                      {employeeName} {surname}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
+            )}
+
             {/* due date */}
-            <div className="flex items-center gap-[70px]">
-              <div className="flex items-center gap-[6px]">
-                <Calendar />
-                <span className="text-[16px] text-firaGo">დავალების ვადა</span>
+            {due_date && (
+              <div className="flex items-center gap-[70px]">
+                <div className="flex items-center gap-[6px] w-[164px]">
+                  <CalendarIcon />
+                  <span className="text-[16px] text-firaGo leading-[150%] font-normal">
+                    დავალების ვადა
+                  </span>
+                </div>
+                <span className="text-gray-blackish text-[14px] text-firaGo leading-[150%]">
+                  {formatDueDateWithWeekday(due_date)}
+                </span>
               </div>
-            </div>
+            )}
           </div>
         </div>
       </div>
+      <CommentSection taskId={id} />
     </div>
   );
 }

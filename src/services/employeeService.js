c413c3d5ced466a53b919cd8a1d7ed7d25995e91
Lika <lika.likuna2008@gmail.com>
2025-03-19 14:59:23 +0400

    feat: fetched employees after adding new one

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -1,12 +1,18 @@
 import axios from "./axios";
+import useEmployeeStore from "../stores/EmployeeStore";
 
 export const addEmployee = async (employee) => {
+  const { fetchEmployees } = useEmployeeStore.getState();
+
   try {
     const response = await axios.post("/employees", employee, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
     });
+
+    await fetchEmployees();
+
     return response.data;
   } catch (error) {
     throw new Error("Error adding employee");

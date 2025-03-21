197eeaccf408faac00dbd7d7d0565cffe905a5d4
Lika <lika.likuna2008@gmail.com>
2025-03-21 18:16:37 +0400

    task: replaced ArrowDown with SVG icon in Selector component

--- src/components/common/Selector/index.jsx
+++ src/components/common/Selector/index.jsx
@@ -79,7 +79,22 @@ export default function Selector({
               აირჩიე
             </span>
           )}
-          <ArrowDown />
+          <svg
+            className={`w-4 h-4 transform transition-transform duration-200 ${
+              isOpen ? "rotate-180" : ""
+            }`}
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M19 9l-7 7-7-7"
+            />
+          </svg>
         </button>
 
         {/* Dropdown options */}
